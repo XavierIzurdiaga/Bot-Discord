eb3e868943a1fb9fa3676ef46ee82a44d6513bf6
“Xizurdigar” <93376356+Xizurdigar@users.noreply.github.com>
2022-04-12 12:40:09 +0200

    Añadida función para limpiar la lista de canciones y solucion de problemas en otras funciones

--- comandos/musica/continuarMusica.js
+++ comandos/musica/continuarMusica.js
@@ -3,10 +3,18 @@ const { enviarMensaje } = require("../mensajeNormal");
 
 module.exports = {continuarMusica};
 
-function continuarMusica(mensaje, player, conexion) {
+function continuarMusica(mensaje, player, conexion, canciones) {
     try{
         if(!conexion[0]){
             enviarMensaje(mensaje, "Debo estar en un canal de voz para desconectarme")
+            return;
+        }
+        if(player.state.status == "playing"){
+            enviarMensaje(mensaje, "La reproduccion de música ya esta reanudada")
+            return;
+        }
+        if(canciones.length == 0){
+            enviarMensaje(mensaje, "No hay canciones para reanudar la reproduccion de musica")
             return;
         }
         player.unpause();
---
+++ comandos/musica/limpiarLista.js
@@ -0,0 +1,26 @@
+const { escribirMensajeError } = require("../mensajeError");
+const { enviarMensaje } = require("../mensajeNormal");
+
+module.exports = {limpiarLista};
+
+function limpiarLista(mensaje, canciones, conexion, player) {
+    try{
+        if(!conexion[0]){
+            enviarMensaje(mensaje, "Debo estar en un canal de voz para limpiar la lista")
+            return;
+        }
+        if(canciones.length == 0){
+            enviarMensaje(mensaje, "La lista ya esta vacía")
+            return;
+        }
+
+        canciones.splice(0);
+        player.stop()
+        // console.log(player.state.status == "playing");
+        
+        enviarMensaje(mensaje, "Lista de canciones limpia")
+    }catch (error) {
+        console.log(error);
+        escribirMensajeError(mensaje);
+    }
+}
